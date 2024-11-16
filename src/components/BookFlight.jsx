0884242f4b45e838f60392dd219fdc9a6ad57dd1
bokhubot <tuandinamite@gmail.com>
2024-11-17 04:17:47 +0700

    update mybooking tab

--- src/components/BookFlight.jsx
+++ src/components/BookFlight.jsx
@@ -6,10 +6,19 @@ function BookFlight() {
     const [to, setTo] = useState("");
     const [depart, setDepart] = useState("");
     const [returnDate, setReturnDate] = useState("");
+    const [ticketNumber, setTicketNumber] = useState("");
+    const [lastName, setLastName] = useState("");
     const [showCalendar, setShowCalendar] = useState(false);
 
     function handleMenuOptionClicked(option) {
         setMenuOption(option);
+    }
+
+    function handleSwitchFromTo() {
+        if (from === "" || to === "") return;
+        const temp = from;
+        setFrom(to);
+        setTo(temp);
     }
 
     return (
@@ -40,9 +49,11 @@ function BookFlight() {
                 <div className="p-4 bg-white">
                     {menuOption === "Book" ? (
                         <div className="flex gap-5">
-                            <ul className="flex flex-1 gap-2">
-                                <li className="flex flex-col border grow">
-                                    <label className="">From</label>
+                            <ul className="flex grow gap-2 items-center">
+                                <li className="flex flex-col border grow p-2">
+                                    <label className="text-theme-outline">
+                                        From
+                                    </label>
                                     <input
                                         type="text"
                                         value={from}
@@ -51,25 +62,36 @@ function BookFlight() {
                                         }
                                     ></input>
                                 </li>
-                                <button className="bg-theme-inversePrimary rounded-full h-min px-2 py-1">&#8644;</button>
-                                <li className="flex flex-col border grow">
-                                    <label className="">To</label>
+                                <button
+                                    className="bg-theme-inversePrimary rounded-full h-min px-2 py-1"
+                                    onClick={handleSwitchFromTo}
+                                >
+                                    &#8644;
+                                </button>
+                                <li className="flex flex-col border grow p-2">
+                                    <label className="text-theme-outline">
+                                        To
+                                    </label>
                                     <input
                                         type="text"
                                         value={to}
                                         onChange={(e) => setTo(e.target.value)}
                                     ></input>
                                 </li>
-                                <li className="flex flex-col border grow">
-                                    <label className="">Depart</label>
+                                <li className="flex flex-col border grow p-2">
+                                    <label className="text-theme-outline">
+                                        Depart
+                                    </label>
                                     <input
                                         type="text"
                                         value={depart}
                                         readOnly
                                     ></input>
                                 </li>
-                                <li className="flex flex-col border grow">
-                                    <label className="">Return</label>
+                                <li className="flex flex-col border grow p-2">
+                                    <label className="text-theme-outline">
+                                        Return
+                                    </label>
                                     <input
                                         type="text"
                                         value={returnDate}
@@ -82,7 +104,33 @@ function BookFlight() {
                             </button>
                         </div>
                     ) : (
-                        <></>
+                        <div className="flex gap-5">
+                            <ul className="flex grow gap-2 items-center">
+                                <li className="border flex flex-col grow p-2">
+                                    <label className="text-theme-outline">Ticket number</label>
+                                    <input
+                                        type="text"
+                                        value={ticketNumber}
+                                        onChange={(e) =>
+                                            setTicketNumber(e.target.value)
+                                        }
+                                    ></input>
+                                </li>
+                                <li className="border flex flex-col grow p-2">
+                                <label className="text-theme-outline">Ticket number</label>
+                                    <input
+                                        type="text"
+                                        value={lastName}
+                                        onChange={(e) =>
+                                            setLastName(e.target.value)
+                                        }
+                                    ></input>
+                                </li>
+                            </ul>
+                            <button className="bg-theme-secondary text-theme-onSecondary font-bold rounded-xl p-4 hover:bg-theme-onSecondaryFixed">
+                                Search
+                            </button>
+                        </div>
                     )}
                 </div>
             </div>
